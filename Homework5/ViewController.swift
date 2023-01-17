77025bd85259b2511aada1663fc5cfb981393ca1
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-18 00:10:31 +0300

    Initial Commit

---
+++ Homework5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework5
+//
+//  Created by Berkay Tuncel on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
