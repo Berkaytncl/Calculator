d5b0fc8c40d16c3dfc7acca3030aa5c80aac613a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-19 02:14:41 +0300

    Create calculator model

---
+++ Homework5/Models/Calculator.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator.swift
+//  Homework5
+//
+//  Created by Berkay Tuncel on 18.01.2023.
+//
+
+import Foundation
+
+struct Calculator {
+    var total: Double
+    var tempNumb: Double
+    var plusPressed: Bool
+    var plusJustPressed: Bool
+    var equalJustPressed: Bool
+    var dotPressed: Bool
+}
