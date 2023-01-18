35ee694633886081cceeec4d4897963008ef076f
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-19 02:18:20 +0300

    Create consts

--- Homework5/ViewController.swift
+++ Homework5/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+struct Consts {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let align: CGFloat = 16
+    static let spacing: CGFloat = 12
+    static let allClear: String = "AC"
+    static let clear: String = "C"
+}
 
     override func viewDidLoad() {
         super.viewDidLoad()
